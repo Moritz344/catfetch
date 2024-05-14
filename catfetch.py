ff41b0240d6948f86767165e244bc14b5211872f
Moritz344 <moritzmaier34@gmail.com>
2024-05-14 15:15:49 +0200

    update 1

--- catfetch.py
+++ catfetch.py
@@ -1,6 +1,7 @@
 from termcolor import cprint
 import platform
 import psutil
+import cpuinfo
 # nfetch 
 # ascii art
 # catpfetch
@@ -15,11 +16,17 @@ def ascii():
     memory_info = psutil.virtual_memory()
     total_memory = memory_info.total
     available_memory = memory_info.available
+    # cpu
+    cpu_info = cpuinfo.get_cpu_info()
+
+    # Prozessornamen
+    processor_name = cpu_info['brand_raw']
     return {
         "Operating System": os_info,
         "Processor": processor,
         "Total Memory": total_memory,
-        "Available Memory": available_memory
+        "Available Memory": available_memory,
+        "CPU": processor_name
     }
 cat = """
       
